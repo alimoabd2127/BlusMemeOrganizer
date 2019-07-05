0467d916cc001474759b6747c293e03f72957aca
Abdulaziz Alimov <alimoabd2127@gmail.com>
2019-07-05 06:07:33 -0400

    Version 1.0

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,38 +1,28 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
 chrome.contextMenus.create({
     "title": "Download Image",
     "contexts": ["image"],
     "onclick": info => {
-        sendDetailsToPopup(info);
         downloadImage(info);
     }
 });
 
-function sendDetailsToPopup(details) {
-    chrome.runtime.sendMessage({
-        msg: "something_completed",
-        data: {
-            subject: "Loading",
-            content: details.srcUrl
-        }
-    });
-}
-
 function downloadImage(details) {
-    chrome.downloads.download({
-        url : details.srcUrl
-    })
+    let re = /(?:\.([^.]+))?$/;
+    chrome.storage.local.get({
+        downloadFolder: "",
+        enablePrompt: false,
+        fileNumber: 0
+    }, function (settings) {
+        settings.fileNumber++;
+        chrome.downloads.download({
+            url : details.srcUrl,
+            filename : settings.downloadFolder + settings.fileNumber + "." + re.exec(details.srcUrl)[1],
+            saveAs : settings.enablePrompt
+        }, function (result) {
+            if(result === undefined){
+                alert("An error occured.  Please check your download path!")
+            }
+        });
+        chrome.storage.local.set({'fileNumber': settings.fileNumber});
+    });
 }
--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,7 +1,79 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.msg === "something_completed") {
-            document.getElementById("content").innerText = request.data.content;
+chrome.storage.local.get('fileNumber', function (result) {
+    if(result.fileNumber != null){
+        let optionalText = "";
+        let tooltipText = "";
+        switch (result.fileNumber) {
+            case result.fileNumber > 100:
+                optionalText = "(Sparked)";
+                tooltipText = "What is this? Rookie hour or something? Keep up the pace!";
+                break;
+            case result.fileNumber > 420:
+                optionalText = "(High)";
+                tooltipText = "You officially know what dank means.";
+                break;
+            case result.fileNumber > 500:
+                optionalText = "(Stoned)";
+                tooltipText = "Keep it up, you're not a beginner anymore.";
+                break;
+            case result.fileNumber > 1000:
+                optionalText = "(Baked)";
+                tooltipText = "You've reached collector status. You can differentiate between trash and dank.";
+                break;
+            case result.fileNumber > 2000:
+                optionalText = "(Toasted)";
+                tooltipText = "Keep climbing the hill. Your skills have vastly improved.";
+                break;
+            case result.fileNumber > 3000:
+                optionalText = "(Smashed)";
+                tooltipText = "A third of the way there already! You don't even associate with normies anymore.";
+                break;
+            case result.fileNumber > 4000:
+                optionalText = "(Blazed)";
+                tooltipText = "You have reached 99 percentile already.  Keep it up and you may be able to reach enlightenment!";
+                break;
+            case result.fileNumber > 5000:
+                optionalText = "(Gone)";
+                tooltipText = "Already halfway there.  You are the definition of a mad-lad.  Snoop Dogg would be proud.";
+                break;
+            case result.fileNumber > 10000:
+                optionalText = "(Out of this Universe)";
+                tooltipText = "You've reached something very few will ever achieve in their lifetime. Congrats!'.";
+                break;
+            default:
+                optionalText = "(Sober)";
+                tooltipText = "You've just started your dank memeing career. Start downloading!";
+                break;
         }
+        document.getElementById("total").innerHTML = "Total downloaded: " + result.fileNumber + " " + optionalText;
+        document.getElementById("total").title = tooltipText;
     }
-);
+});
+
+function save_options() {
+    let folder = document.getElementById('downloadFolder').value;
+    let prompt = document.getElementById('enablePrompt').checked;
+    chrome.storage.local.set({
+        downloadFolder: folder,
+        enablePrompt: prompt
+    }, function() {
+        let status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+function restore_options() {
+    // Use default value color = 'red' and enablePrompt = true.
+    chrome.storage.local.get({
+        downloadFolder: "",
+        enablePrompt: false
+    }, function(items) {
+        document.getElementById('downloadFolder').value = items.downloadFolder;
+        document.getElementById('enablePrompt').checked = items.enablePrompt;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
